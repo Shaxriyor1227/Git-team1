18c155f5c638784993f2e404a9d7c7c49e1e503e
ibrohim-valixanov-web <ibrohimvalixanov778@gmail.com>
2026-04-10 11:54:18 +0500

    ibrohim

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,17 @@ import React from 'react'
 import About from './components/about/About'
 import Header from './components/header/Header'
 import Main from './components/main/Main'
+import About2 from './components/about/About2'
 
 const App = () => {
   return (
     <div>
-      <About />
       <Header />
+      <About />
+
       <Main />
+      <About2/> 
+
     </div>
   )
 }
